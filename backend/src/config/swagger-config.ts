d6a63a826d3b00a2ecc7a1f4ce9b06d87d57aec2
tim <tim.francois@student.kdg.be>
2021-04-26 13:29:36 +0200

    initial commit frontend

---
+++ backend/src/config/swagger-config.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+const configSwagger = (app: INestApplication): void => {
+    const config = new DocumentBuilder()
+        .setTitle('Kudo-Box-v2')
+        .setDescription('Kudo-Box API documentation')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+}
+
+export default configSwagger;
