f802b8c7e5e8ad9f5e3d60ef189c6b8bf2b8e4a6
lenne <lennertmoorthamer2@hotmail.com>
2021-05-03 16:55:49 +0200

    axios implementation

--- frontend/src/pages/NewKudo.tsx
+++ frontend/src/pages/NewKudo.tsx
@@ -2,14 +2,16 @@ import Navbar from '../components/Navbar'
 import CreateKudoBar from '../components/CreateKudoBar'
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
 import { EmojiEmotions } from '@material-ui/icons';
+import axios from '../services/Axios';
 import classes from '../styles/NewKudo.module.scss';
 
 export default function NewKudo() {
     const [theme, setTheme] = useState(null);
+    const [createdKudo, setCreatedKudo] = useState(null);
     const [kudoText, setKudoText] = useState("");
     const [emojiPopup, setEmojiPopup] = useState(null);
     const canvas = useRef(null);
@@ -32,16 +34,34 @@ export default function NewKudo() {
         setEmojiPopup(!emojiPopup);
     };
 
-    // useEffect(() => {
-    //     if (theme != null) {
-    //         const image = new Image();
-    //         image.src = theme;
-    //         const ctx = canvas.current.getContext('2d');
-    //         image.onload = () => {
-    //             ctx.drawImage(image, 0, 0, canvas.current.width, canvas.current.height);
-    //         }
-    //     }
-    // }, [theme])
+    const createKudo = async () => {
+        const ctx = canvas.current.getContext('2d');
+        ctx.fillText(kudoText, 80, 150, 150);
+        const imageUrl = canvas.current.toDataURL('image/webp');
+
+        const formData = new FormData();
+        formData.append('kudoImage', new File([imageUrl], "kudo.webp"));
+        formData.append('senderId', "bdd002c9-51d9-4bea-a48a-46cc46eab912");
+        formData.append('receiverId', "05983dd0-1995-4697-b741-4154ee945e7f");
+
+        console.log(formData)
+        await axios.post(
+            '/kudo/create', formData,
+            false
+        );
+    }
+
+    useEffect(() => {
+        if (theme != null) {
+            const image = new Image();
+            image.src = theme;
+            const ctx = canvas.current.getContext('2d');
+            image.onload = () => {
+                ctx.drawImage(image, 0, 0, canvas.current.width, canvas.current.height);
+            }
+            setCreatedKudo(image.src)
+        }
+    }, [theme])
 
     return (
         <>
@@ -50,8 +70,8 @@ export default function NewKudo() {
                 <h1 className={classes.title}>Create Kudo</h1>
                 <CreateKudoBar tab={2} />
                 <div className={classes.image}>
-                    {/* <canvas ref={canvas} id="canvas" className={classes.canvas}></canvas> */}
-                    {theme && <Image src={theme} alt="kudo" layout="fill" />}
+                    <canvas ref={canvas} id="canvas" className={classes.canvas}></canvas>
+                    {/* {theme && <Image src={theme} alt="kudo" layout="fill" />} */}
                     <button
                         className={classes.emojiButton}
                         onClick={onAddEmojiClick}>
@@ -84,7 +104,7 @@ export default function NewKudo() {
                     <a >Cancel</a>
                 </Link>
                 <Link href="/">
-                    <a >Create Kudo</a>
+                    <a onClick={createKudo}>Create Kudo</a>
                 </Link>
             </div>
         </>
---
+++ frontend/src/services/Axios.ts
@@ -0,0 +1,80 @@
+import axiosDependency from 'axios';
+
+const ApiUrl = "http://localhost:3030";
+const timeout = 5000;
+
+/**
+ * Takes a string, replaces all \ with / and ensures it starts with a /
+ *
+ * @param {String} url a cleaned up URL
+ */
+function cleanUrl(url) {
+    if (typeof url !== 'string') {
+        url = url.toString();
+    }
+
+    let cleanBase = url.replaceAll('\\', '/');
+
+    if (!url.startsWith('/')) {
+        cleanBase = '/' + cleanBase;
+    }
+
+    return cleanBase;
+}
+
+/**
+ * A function that contains the default request config to use for axios
+ * see https://github.com/Axios/Axios#request-config
+ * @returns an array with the default request config to use for axios
+ */
+function getBasicRequestParams() {
+    return {
+        baseURL: ApiUrl,
+        timeout: timeout,
+        maxContentLength: -1,
+        maxBodyLength: -1,
+        // responseType: 'document',
+        // headers: {
+        //     'http.content_type': 'application/json',
+        //     // authorization: vuex.getters.getToken
+        // },
+        responseEncoding: 'utf8',
+        validateStatus: function (status) {
+            return status >= 200 && status < 300;
+        },
+        maxRedirects: 10
+    };
+}
+
+/**
+ * Our implementation of axios, will automatically redirect to the error pages if handleOwnError is not set to true
+ */
+export default class axiosImp {
+    static async handleRequest(initialProm, handleOwnError = false) {
+        try {
+            return await initialProm;
+        } catch (error) {
+            if (handleOwnError) throw error;
+        }
+    }
+    static async get(url, handleOwnError = false) {
+        let prom = axiosDependency.get(cleanUrl(url), getBasicRequestParams());
+        return this.handleRequest(prom, handleOwnError);
+    }
+    static async post(url, body = {}, handleOwnError = false) {
+        let prom = axiosDependency.post(cleanUrl(url), body, getBasicRequestParams());
+        return this.handleRequest(prom, handleOwnError);
+    }
+    static async put(url, body = {}, handleOwnError = false) {
+        let prom = axiosDependency.put(cleanUrl(url), body, getBasicRequestParams());
+        return this.handleRequest(prom, handleOwnError);
+    }
+    static async delete(url, handleOwnError = false) {
+        let prom = axiosDependency.delete(cleanUrl(url), getBasicRequestParams());
+        return this.handleRequest(prom, handleOwnError);
+    }
+    static async patch(url, body = {}, handleOwnError = false) {
+        let prom = axiosDependency.patch(cleanUrl(url), body, getBasicRequestParams());
+        return this.handleRequest(prom, handleOwnError);
+    }
+}
