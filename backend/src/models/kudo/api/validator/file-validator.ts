033d04aa6f801ab61312614b12432d35263b0b1c
tim <tim.francois@student.kdg.be>
2021-05-04 15:01:13 +0200

    file validator tests working

--- backend/src/models/kudo/api/validator/file-validator.ts
+++ backend/src/models/kudo/api/validator/file-validator.ts
@@ -1,7 +1,9 @@
 import { BadRequestException } from "@nestjs/common";
 
-export function validateImage(_req: Request, file: Express.Multer.File, cb): void {
-    if(file === null) cb(new BadRequestException('File is required'), false);
+type cbDef = (error: Error | null, acceptFile: boolean) => void;
+
+export function validateImage(_req: Request, file: Express.Multer.File, cb: cbDef): void {
+    if(Object.keys(file).length === 0) cb(new BadRequestException('File is required'), false);
     else if(file.mimetype !== 'image/webp') cb(new BadRequestException('Incorrect file type'), false);
     else cb(null, true);
 }
