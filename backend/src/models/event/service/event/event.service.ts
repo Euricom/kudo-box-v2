39f968f52f02b898364f8f827675ad85c09361e9
tim <tim.francois@student.kdg.be>
2021-05-11 12:01:16 +0200

    get main events for dropdown

--- backend/src/models/event/service/event/event.service.ts
+++ backend/src/models/event/service/event/event.service.ts
@@ -24,6 +24,10 @@ export class EventService extends ImageEntityService<Event> {
     return await this.createImageEntity(event, eventImage);
   }
 
+  async getMainEvents() {
+    return await (this.repo as EventRepository).findMainEvents();
+  }
+
   private async assignMainEvent(childEvent: Event, mainEventId: string): Promise<void> {
     const mainEvent = await (this.repo as EventRepository).findByIdIncludingTags(mainEventId);
     if(!mainEvent) throw new BadRequestException(null, `Main event with id ${mainEventId} not found`);
