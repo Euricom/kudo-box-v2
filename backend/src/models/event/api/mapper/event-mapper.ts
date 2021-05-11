3c1187ac730b8b8339811ac3d13c9b9e748e258e
lenne <lennertmoorthamer2@hotmail.com>
2021-05-11 16:27:54 +0200

    merge conflicts

--- backend/src/models/event/api/mapper/event-mapper.ts
+++ backend/src/models/event/api/mapper/event-mapper.ts
@@ -6,7 +6,6 @@ import { DropDownEventDto } from "../dto/out/DropDownEvent.dto";
 export class EventMapper {
     static fromCreateEventDto(dto: CreateEventDto): Event {
         const host = new User(dto.hostId, undefined, undefined, undefined)
-        
         return new Event(undefined, dto.title, dto.isMainEvent.toLowerCase() === 'true', undefined, undefined, undefined, host);
     }
 
