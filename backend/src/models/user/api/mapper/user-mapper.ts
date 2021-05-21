89c7d9a3fabdcd7173c1d08bd1a207e51408ef77
tim <tim.francois@student.kdg.be>
2021-05-21 11:22:15 +0200

    removed if/else

--- backend/src/models/user/api/mapper/user-mapper.ts
+++ backend/src/models/user/api/mapper/user-mapper.ts
@@ -3,12 +3,9 @@ import { User } from '../../entities/user.entity';
 
 export class UserMapper {
     static fromGraphUser(graphUser: GraphUser): User {
-        let lastName: string;
         let displayNameExistsRegex = new RegExp(`^${graphUser.givenName}`);
 
-        if(displayNameExistsRegex.test(graphUser.displayName)) lastName = graphUser.displayName.replace(`${graphUser.givenName} `, '');
-        else lastName = graphUser.displayName.replace(/^\S+ /, '');
-
-        return new User(graphUser.id, graphUser.givenName, lastName, graphUser.userPrincipalName);
+        if(displayNameExistsRegex.test(graphUser.displayName)) new User(graphUser.id, graphUser.givenName, graphUser.displayName.replace(`${graphUser.givenName} `, '') , graphUser.userPrincipalName);
+        return new User(graphUser.id, graphUser.givenName, graphUser.displayName.replace(/^\S+ /, ''), graphUser.userPrincipalName);
     }
 }
