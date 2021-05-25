0d61bc36f4c23ec5c42a54e65f4c2a4b28ccccc6
lenne <lennertmoorthamer2@hotmail.com>
2021-05-25 16:35:34 +0200

    little typo

--- backend/src/models/user/api/user.controller.ts
+++ backend/src/models/user/api/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Request } from "@nestjs/common";
 import { KudoMapper } from "../../../models/kudo/api/mapper/kudo-mapper";
-import { RequestWithUser } from "../../..//models/utils/api/request-with-user";
-import { ApiDefaultControllerDoc } from "../../..//models/utils/api/swagger/api-default-controller-doc.decorator";
+import { RequestWithUser } from "../../../models/utils/api/request-with-user";
+import { ApiDefaultControllerDoc } from "../../../models/utils/api/swagger/api-default-controller-doc.decorator";
 import { UserService } from "../service/user.service";
 import { MyKudosDto } from "./dto/out/my-kudos.dto";
 
@@ -11,7 +11,7 @@ export class UserController {
     constructor(
         private readonly userService: UserService,
         private readonly kudoMapper: KudoMapper
-    ) {}
+    ) { }
 
     @Get('me/kudos')
     async getLoggedInKudos(@Request() req: RequestWithUser): Promise<MyKudosDto> {
