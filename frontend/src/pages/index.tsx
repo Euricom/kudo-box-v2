4d4e20b99a3554aae38c1d1c98686ffd197591e2
lenne <lennertmoorthamer2@hotmail.com>
2021-06-04 16:06:56 +0200

    newKudoCta

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ import EventsList from '../components/EventList/EventList'
 import classes from '../styles/index.module.scss';
 import { Event } from '../domain'
 import { useHttpEventClient } from '../hooks/clients'
+import Link from 'next/link'
 
 interface Props {
     fetchedEvents: Event[]
@@ -24,7 +25,7 @@ export default function Home({ }: Props) {
     }, [])
 
     return (
-        <div>
+        <>
             <Navbar />
             <h1>Home</h1>
             <div className={classes.eventsHolder}>
@@ -37,7 +38,10 @@ export default function Home({ }: Props) {
                     </div>
                 }
             </div>
-        </div>
+            <Link href="/ChooseTheme">
+                <a className={classes.newKudoCta}>Create Kudo</a>
+            </Link>
+        </>
     );
 }
 
