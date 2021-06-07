3134c83603d53f4b0b6d4b2dc89079e83ea8ab5d
lenne <lennertmoorthamer2@hotmail.com>
2021-06-07 10:16:10 +0200

    handled eventToggle frontend

--- frontend/src/components/EventList/EventList.tsx
+++ frontend/src/components/EventList/EventList.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import { useRouter } from 'next/router'
 import { Event } from "../../domain"
+import { Lock, LockOpen } from '@material-ui/icons';
+import { useHttpEventClient } from '../../hooks/clients'
 import classes from './EventList.module.scss';
 
 interface EventListData {
@@ -10,26 +12,41 @@ interface EventListData {
 
 export default function EventList(props: EventListData) {
     const router = useRouter()
+    const { toggleActive } = useHttpEventClient();
 
     const handleEventClick = (id: string, title: string) => {
         router.push({
             pathname: '/ChooseTheme',
-            query: { eventId: id, eventTitle: title}
+            query: { eventId: id, eventTitle: title }
         })
     }
+
+    const onLockclick = async (id: string) => {
+        await toggleActive(id);
+        router.push('/');
+    }
+
     return (
         <>
             {
                 props.events.map((event, index) => {
-                    return <div key={`${event.id}.${index}`} onClick={() => handleEventClick(event.id, event.title)} className={classes.eventHolder}>
-                        <div className={classes.imageHolder}>
-                            <Image src={atob(event.eventImage)} alt="kudo" layout="fill" />
+                    return <div key={`${event.id}.${index}`} className={classes.eventHolder}>
+                        <div onClick={() => handleEventClick(event.id, event.title)} className={classes.contentHolder}>
+                            <div className={classes.imageHolder}>
+                                <Image src={atob(event.eventImage)} alt="kudo" layout="fill" />
+                            </div>
+                            <div className={classes.infoHolder}>
+                                <p className={classes.tabbed}>{event.title}</p>
+                                <p>{`# ${event.tagName}`}</p>
+                                <p>{event.creationDate}</p>
+                            </div>
                         </div>
-                        <div className={classes.infoHolder}>
-                            <p className={classes.tabbed}>{event.title}</p>
-                            <p>{`# ${event.tagName}`}</p>
-                            <p>{event.creationDate}</p>
-                        </div>
+                        <button
+                            className={classes.buttonHolder}
+                            onClick={() => onLockclick(event.id)}>
+                            {!event.active && <Lock className={classes.icon} />}
+                            {event.active && <LockOpen className={classes.icon} />}
+                        </button>
                     </div>
                 })
             }
