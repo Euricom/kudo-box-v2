aeef4866e042375173334a7d268fb5bc6279f1b6
tim <tim.francois@student.kdg.be>
2021-06-11 10:08:48 +0200

    ws config backend

--- backend/src/models/event/api/ws/event-room-gateway.ts
+++ backend/src/models/event/api/ws/event-room-gateway.ts
@@ -9,7 +9,7 @@ import { EventRoomDto } from "../dto/out/EventRoom.dto";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-@WebSocketGateway({ namespace: process.env.WS_EVENT_NAMESPACE })
+@WebSocketGateway(process.env.ENV === 'prod' ? 443 : 80, { namespace: process.env.WS_EVENT_NAMESPACE, allowUpgrades: true, transports: ['websocket'], origins: [process.env.CLIENT_URL] })
 export class EventRoomGateway {
     @WebSocketServer()
     private _server!: Server;
